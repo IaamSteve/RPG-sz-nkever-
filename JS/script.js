6c82d6c71a7cd78d2ce9a84519a6ac9d75c57fb8
KIstvan <kerekesistvan@turr.hu>
2022-10-13 10:56:29 +0200

    new try: edit css in script

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+let red = $('#ammR')
+    green = $('#ammG')
+    blue = $('#ammB')
+
+$('#ammR').change(function(){
+    var style = document.createElement('style')
+    document.head.appendChild(style)
+    style.sheet.insertRule('ammR {color: darkseagreen}')
+})
+
+$('#ammG').change(function(){
+    
+})
+
+$('#ammB').change(function(){
+   
+})
